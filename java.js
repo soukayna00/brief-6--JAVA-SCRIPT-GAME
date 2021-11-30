34acdb5133e2aa137c76e5ca6bf013dcbf4b68e3
soukayna00 <soukayna.zaidi06@gmail.com>
2021-11-30 16:53:12 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,34 @@
+
+//identification
+
+Var random=document ;//Number chosen by the computer 
+Var userimput ;// value chosen by the user
+Var I;// compteur
+var comptrest=10-I// number of the attempts left to the user 
+
+
+1<random<100;
+1<userimput<100;
+1<I<100;
+{    
+    .getElementById("RANDOM").innerHTML = Math.random();
+   
+    for(RANDOM==numuser , compteur<=3 ) ;{
+        you are genius
+    }
+     
+
+     for(RANDOM==numuser , compteur>10 ) ;
+    Printf congratulation
+
+
+     for(random=!numuser , compteur =10 ) ;
+     YOU LOST
+    
+    
+    
+
+
+
+  }
+

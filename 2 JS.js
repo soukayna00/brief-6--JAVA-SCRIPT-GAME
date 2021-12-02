47dbe882801c125d59078967347f43a287ed537c
soukayna00 <soukayna.zaidi06@gmail.com>
2021-12-02 10:42:36 +0100

    final game

--- 2 JS.js
+++ 2 JS.js
@@ -1,25 +1,38 @@
+// number chosen by the computer
+
 var y = Math.floor(Math.random() * 100 + 1);
       
-   
+   // initialistion du compteur 
     var guess = 1;
+    
       
     document.getElementById("submitguess").onclick = function(){
       
    
    var x = document.getElementById("guessField").value;
   
-   if(x == y)
-   {    
-       alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
-               + guess + " GUESS ");
+   if(x == y )
+   {    if(guess<=3){
+        alert(`you are a genius!!! YOU GUESSED IT RIGHT IN ${guess} GUESS `);
+     }
+     else
+     alert(`congratulation!!! YOU GUESSED IT RIGHT IN ${guess} GUESS `);
    }
-   else if(x > y)
+   else if(x > y ,guess<10){
        guess++;
-       alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+       alert(`Try a smaller number!! Your number of guesses is ${guess} `);
    }
-   else
+   else if (x<y , guess<10)
    {
        guess++;
-       alert("OOPS SORRY!! TRY A GREATER NUMBER")
+       alert(`Try A higher number!! Your number of guesses is ${guess} `);
+   }
+   else if( x=!y , guess=10 )
+   {    
+       alert(`YOU lost your 10 attempt, restart the game `);
+      
+
+
    }
 }
+  
